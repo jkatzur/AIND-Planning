fb64b2729f924668cf14a990c7b584868ae8050e
Jon Katzur <jkatzur@gmail.com>
2017-09-06 22:27:49 -0400

    competing needs mutex complete

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -447,8 +447,10 @@ class PlanningGraph():
         :param node_a2: PgNode_a
         :return: bool
         """
-
-        # TODO test for Competing Needs between nodes
+        for p1 in node_a1.parents:
+            for p2 in node_a2.parents:
+                #Not sure if mutex is always reflexive - I think so
+                if p1.is_mutex(p2) or p2.is_mutex(p1): return True
         return False
 
     def update_s_mutex(self, nodeset: set):

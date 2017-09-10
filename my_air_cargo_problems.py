898992a95d96dfb8512f5328a79fdc58492a5522
Jon Katzur <jkatzur@gmail.com>
2017-09-10 18:00:26 -0400

    Fixed issue in h_ignore precond

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -8,7 +8,7 @@ from lp_utils import (
     FluentState, encode_state, decode_state,
 )
 from my_planning_graph import PlanningGraph
-
+import ipdb
 from functools import lru_cache
 
 
@@ -216,7 +216,9 @@ class AirCargoProblem(Problem):
         # Create a Propositional-logic KB that will be used to map the existing paths
         kb = PropKB()
         # This structures the path data in a way where we can determine if a goal clause is true
-        kb.tell(decode_state(node.path(), self.state_map).pos_sentence())
+        # I need to switch this over
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
+        #ipdb.set_trace()
         # For each part of the goal...
         for clause in self.goal:
             # If that part is not answered in the existing path - add 1 (e.g count this as a "miss")
